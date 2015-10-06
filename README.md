bb75be88cc1918f185ccba94781a899e349b341a
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-10-06 10:09:44 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple php templates parser
---
+++ src/Parser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Templating;
+
+class Parser {
+
+    private $path;
+    private $vars;
+
+    public function __construct($path,$vars = []) {
+        $this->path = $path;
+        foreach ($vars as $key => $value) {
+            $this->vars[$key] = $value;
+        }
+    }
+
+    public function render($template,array $values = []) {
+        extract($this->vars);
+        if (count($values) > 0) extract($values);
+        ob_start();
+        $fullPath = $this->path.$template;
+        if (file_exists($fullPath)) include $fullPath;
+        return ob_get_clean();
+    }
+
+}
