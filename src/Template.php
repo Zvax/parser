77189b4c5fb73ef053425d692e6064bc9c761f6f
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-12-09 16:39:05 -0500

    better not start ob before throwing

--- src/Template.php
+++ src/Template.php
@@ -7,10 +7,10 @@ use Templating\Exceptions\InvalidFileException;
 abstract class Template implements Renderer {
     public function render($template, $value = null)
     {
-        ob_start();
         if (!file_exists($template)) {
             throw new InvalidFileException($template);
         }
+        ob_start();
         require $template;
         return ob_get_clean();
     }
