288eb08b7645b4036db2f38f00d96c3a600d8a6a
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-12-11 14:44:29 -0500

    rendering from arrays

--- src/PhpTemplatesRenderer.php
+++ src/PhpTemplatesRenderer.php
@@ -16,9 +16,24 @@ class PhpTemplatesRenderer implements Renderer
         $scope = function() use ($template) { require $template; };
         if ($value !== null)
         {
+            if (is_array($value))
+            {
+                $value = $this->makeObject($value);
+            }
             $scope = $scope->bindTo($value);
         }
         $scope();
         return ob_get_clean();
     }
+
+    private function makeObject(array $value)
+    {
+        $newValue = new \stdClass();
+        foreach ($value as $key => $content)
+        {
+            $newValue->$key = $content;
+        }
+        return $newValue;
+    }
+
 }
