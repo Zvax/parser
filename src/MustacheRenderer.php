30105de59f7a12cf96a7ea2d3e8c96140afe5c1b
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-01-04 15:55:52 -0500

    introducing mustache

---
+++ src/MustacheRenderer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Templating;
+
+use Mustache_Engine;
+
+class MustacheRenderer implements Renderer
+{
+    private $Renderer;
+
+    public function __construct(Mustache_Engine $oMustache)
+    {
+        $this->Renderer = $oMustache;
+    }
+
+    public function render($template, $values = [])
+    {
+        return $this->Renderer->render($template, $values);
+    }
+}
