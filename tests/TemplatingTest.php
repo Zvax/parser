1c9a51caca8fa3cae330d1d8a759b9bbe2ad07a8
Félix Gagnon-Grenier <fractalesque@gmail.com>
2022-05-19 17:05:40 -0400

    [chore] updates phpunit

--- tests/TemplatingTest.php
+++ tests/TemplatingTest.php
@@ -1,48 +1,56 @@
 <?php
+
 namespace Tests;
+
 use PHPUnit\Framework\TestCase;
 use Storage\FileLoader;
 use Templating\PhpTemplatesRenderer;
+
 class TemplatingTest extends TestCase
 {
     public function testCanAccessRendererFromTemplate()
     {
-        $loader = new FileLoader(__DIR__.'/templates/', 'php');
+        $loader = new FileLoader(__DIR__ . '/templates/', 'php');
         $renderer = new PhpTemplatesRenderer($loader);
         $string = $renderer->render('accesses_renderer');
-        $this->assertEquals('value'.PHP_EOL, $string);
+        $this->assertEquals('value' . PHP_EOL, $string);
     }
-    public function testAccessesValuesFromSubTemplate ()
+
+    public function testAccessesValuesFromSubTemplate()
     {
-        $loader = new FileLoader(__DIR__.'/templates/', 'php');
+        $loader = new FileLoader(__DIR__ . '/templates/', 'php');
         $renderer = new PhpTemplatesRenderer($loader);
         $string = $renderer->render('accesses_renderer_with_values', [
             'value' => 'value',
         ]);
         $this->assertEquals('value', $string);
     }
+
     public function testRenders()
     {
         $view = new ExampleTemplate;
         $this->assertInstanceOf('Tests\ExampleTemplate', $view);
         $string = $view->render(__DIR__ . '/templates/test_template.php');
-        $this->assertInternalType('string', $string);
+        $this->assertIsString($string);
     }
+
     public function testException()
     {
         $this->expectException('Templating\Exceptions\InvalidFileException');
         $view = new ExampleTemplate;
         $view->render('nonfile');
     }
+
     public function testAcceptsObjectAndRendersIt()
     {
         $view = new ExampleViewObject;
         $loader = new FileLoader(__DIR__ . "/templates", "php");
         $renderer = new PhpTemplatesRenderer($loader);
         $string = $renderer->render('test_template', $view);
-        $this->assertInternalType('string', $string);
-        $this->assertContains('default body', $string);
+        $this->assertIsString($string);
+        $this->assertStringContainsString('default body', $string);
     }
+
     public function testAcceptsArrayAndRendersValues()
     {
         $loader = new FileLoader(__DIR__ . "/templates", "php");
@@ -53,9 +61,10 @@ class TemplatingTest extends TestCase
                 'body' => 'body from array',
             ]
         );
-        $this->assertInternalType('string', $string);
-        $this->assertContains('body from array', $string);
+        $this->assertIsString($string);
+        $this->assertStringContainsString('body from array', $string);
     }
+
     public function testRendersFromBasePath()
     {
         $loader = new FileLoader(__DIR__ . "/templates", "php");
@@ -66,7 +75,7 @@ class TemplatingTest extends TestCase
                 'body' => 'whatever',
             ]
         );
-        $this->assertInternalType('string', $string);
-        $this->assertContains('whatever', $string);
+        $this->assertIsString($string);
+        $this->assertStringContainsString('whatever', $string);
     }
 }
